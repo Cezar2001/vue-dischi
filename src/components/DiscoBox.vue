f03b80c7fd593f47da1fa4beb4dedebd4414bc5d
Cezar <cezarlantosgmail.com>
2022-01-31 13:30:30 +0100

    errore con props

---
+++ src/components/DiscoBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img :src="disco.poster" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        disco: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    div{
+        width: 100px;
+        height: 100px;
+    }
+</style>
---
+++ src/components/MainContainer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <disco-box v-for="(disco, index) in discoList" 
+        :key="index" 
+        :disco="disco"/>
+    </div>
+</template>
+
+<script>
+import DiscoBox from './DiscoBox.vue'; 
+export default {
+    components: {
+        DiscoBox
+    },
+    props: {
+        discoList: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+div{
+    // display: flex;
+    // flex-wrap: wrap;
+    // justify-content: space-around;
+    // text-align: center;
+    // padding: 25px;
+    height: 150px;
+}
+</style>
